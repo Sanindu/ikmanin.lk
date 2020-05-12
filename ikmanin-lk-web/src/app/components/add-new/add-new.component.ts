cd5f9592bbd9f901817f7494d82543960b83a74a
Sanindu <rathnayakesanindu99@gmail.com>
2020-05-12 09:52:52 +0530

    New Ad Form

--- ikmanin-lk-web/src/app/components/add-new/add-new.component.ts
+++ ikmanin-lk-web/src/app/components/add-new/add-new.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Advertisement } from 'src/app/data-models/Advertisement';
 import {Location} from 'src/app/data-models/Location';
-import {AddNewService } from 'src/app/services/advertisement-services/add-new.service';
-
 
 
 
@@ -17,18 +15,11 @@ userModel = new Advertisement();
 locationModel = new Location();
 
  onSubmit(){
-   this.addnewservice.submitForm1(this.userModel).subscribe (
-     data => console.log('Sucess !' , data),
-     error => console.error('Error', error)
- );
-
-   this.addnewservice.submitFormlocation(this.locationModel).subscribe (
-  data => console.log('Sucess !' , data),
-  error => console.error('Error', error)
-);
+   console.log(this.userModel);
+   console.log(this.locationModel);
 
  }
-  constructor( private addnewservice: AddNewService) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
